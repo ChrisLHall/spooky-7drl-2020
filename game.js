f3ecb975bedc1174e51dc52a1301ac2c69dfbd63
ChrisLHall <mrchrislhall@gmail.com>
2020-03-04 00:06:54 -0800

    The most bare semblance of a game loop. Can go down levels, can get hurt. Cannot die. Getting hurt looks dumb.

--- game.js
+++ game.js
@@ -6,10 +6,14 @@ var CAM_HEIGHT = 11;
 var camX = 0;
 var camY = 0;
 
+var level = 1;
+
 var you = {
   gfx: "✌",
   x: 5,
   y: 5,
+  health: 5,
+  maxHealth: 5,
   hasKey: false,
 };
 var keyObj = null;
@@ -42,12 +46,14 @@ var entityTypeToGfx = {
   "rat": [["🐁"]],
   "slime": [["⚛","⚛"],
             ["⚛","⚛"]],
-  
 }
+
 // todo moving+placing big entities
 var entityUpdateFunctions = {
   "key": updateKey,
+  "exit": updateExit,
   "rat": updateMoveRandom,
+  "slime": updateMoveRandom,
 };
 
 function choose(arr) {
@@ -65,6 +71,18 @@ function randomInt(min, max) {
 // shallow copy
 function copyFromTemplate(obj, template) {
   return Object.assign(obj, template); // test??
+}
+
+function entityWidth(entity) {
+  return entity.gfx[0].length;
+}
+
+function entityHeight(entity) {
+  return entity.gfx.length;
+}
+
+function entityIsHostile(entity) {
+  return entity.type === "rat" || entity.type === "slime";
 }
 
 var FLOOR_TILES = ["▫", "⬜", "⚪"]; // todo
@@ -124,7 +142,7 @@ function placeKey(entities, xMin, yMin, xMax, yMax) {
     // try to place 100 times
     var x = randomInt(xMin, xMax);
     var y = randomInt(yMin, yMax);
-    if (isEmpty(x, y)) {
+    if (!isWall(x, y)) {
       keyObj = copyFromTemplate({}, entityTemplate);
       keyObj.type = "key";
       keyObj.gfx = entityTypeToGfx["key"];
@@ -143,7 +161,7 @@ function placeExit(entities, xMin, yMin, xMax, yMax, minKeyDist) {
     var x = randomInt(xMin, xMax);
     var y = randomInt(yMin, yMax);
     var keyDist = Math.abs(x - keyObj.x) + Math.abs(y - keyObj.y);
-    if (keyDist > minKeyDist && isEmpty(x, y)) {
+    if (keyDist > minKeyDist && !isWall(x, y)) {
       exitObj = copyFromTemplate({}, entityTemplate);
       exitObj.type = "exit";
       exitObj.gfx = entityTypeToGfx["exit"];
@@ -158,32 +176,43 @@ function placeExit(entities, xMin, yMin, xMax, yMax, minKeyDist) {
 
 function placeMonsters(entities, count, type) {
   for (var j = 0; j < count; j++) {
-    for (var attempt = 0; attempt < 10; attempt++) {
+    var monster = copyFromTemplate({}, entityTemplate);
+    monster.type = type;
+    monster.gfx = entityTypeToGfx[type];
+    monster.x = 5;
+    monster.y = 5;
+    var width = entityWidth(monster);
+    var height = entityHeight(monster);
+    for (var attempt = 0; attempt < 100; attempt++) {
       // try to place 10 times
       var x = randomInt(0, WIDTH);
       var y = randomInt(0, HEIGHT);
-      if (isEmpty(x, y)) {
-        var monster = copyFromTemplate({}, entityTemplate);
-        monster.type = type;
-        monster.gfx = entityTypeToGfx[type];
+      if (!isWall(x, y, width, height) && entitiesAt(x, y, width, height).length === 0) {
         monster.x = x;
         monster.y = y;
-  
-        entities.push(monster);
         break;
       }
     }
+    entities.push(monster);
   }
 }
 
-// ENTRY POINT
-function startGame() {
+function buildLevel(level) {
   bg = generateBG(WIDTH, HEIGHT);
   environment = generateWalls(WIDTH, HEIGHT);
   entities = [];
   placeKey(entities, 3, 3, WIDTH - 3, HEIGHT - 3);
   placeExit(entities, 3, 3, WIDTH - 3, HEIGHT - 3, 10);
   placeMonsters(entities, 15, "rat");
+  placeMonsters(entities, 2, "slime");
+  you.x = 0;
+  you.y = 0;
+  // todo place YOU
+}
+
+// ENTRY POINT
+function startGame() {
+  buildLevel(1);
 
   renderGrid();
 }
@@ -237,7 +266,6 @@ function renderGrid() {
     frame.push(frameRow);
   }
 
-  // TODO draw YOU and entities
   for (var idx = 0; idx < entities.length; idx++) {
     var entity = entities[idx];
     for (var j = 0; j < entity.gfx.length; j++) {
@@ -266,19 +294,53 @@ function renderGrid() {
     str += "<br>";
   }
   // TODO health?
-  str += "💙💙💙💙💙" + "&nbsp;&nbsp;&nbsp;" + (you.hasKey ? "🗝" : "&nbsp;") + "<br>";
+  var healthStr = "";
+  for (var j = 0; j < you.maxHealth; j++) {
+    healthStr += you.health > j ? "💙" : "🖤";
+  }
+  str += "Level " + level + "&nbsp;&nbsp;&nbsp;" + healthStr + "&nbsp;&nbsp;&nbsp;" + (you.hasKey ? "🗝" : "&nbsp;") + "<br>";
   grid.innerHTML = twemoji.parse(str);
 }
 
-function isEmpty(x, y) {
-  if (x < 0 || y < 0 || x >= WIDTH || y >= HEIGHT) return false;
-  if (you.x === x && you.y === y) return false;
-  if (environment[y][x]) return false;
-  return true;
+function isWall(x, y, width, height) {
+  width = width || 1;
+  height = height || 1;
+  if (x < 0 || y < 0 || x + width > WIDTH || y + height > HEIGHT) return true;
+
+  for (var j = 0; j < height; j++) {
+    for (var k = 0; k < width; k++) {
+      if (environment[y + j][x + k]) return true;
+    }
+  }
+  return false;
+}
+
+function isYou(x, y, width, height) {
+  width = width || 1;
+  height = height || 1;
+  
+  return (you.x >= x && you.x < x + width && you.y >= y && you.y < y + height);
+}
+
+function entitiesAt(x, y, width, height, self) {
+  var result = [];
+  width = width || 1;
+  height = height || 1;
+  self = self || null;
+
+  for (var j = 0; j < entities.length; j++) {
+    var entity = entities[j];
+    if (entity === self) continue;
+    if (x < entity.x + entityWidth(entity) && entity.x < x + width
+        && y < entity.y + entityHeight(entity) && entity.y < y + height) {
+      result.push(entity);
+    }
+  }
+  return result;
 }
 
 function tryMovePlayer(dx, dy) {
-  if (isEmpty(you.x + dx, you.y + dy)) {
+  if (!isWall(you.x + dx, you.y + dy)) {
     you.x += dx;
     you.y += dy;
   }
@@ -287,21 +349,36 @@ function tryMovePlayer(dx, dy) {
 // entity update functions //
 
 function updateKey(key) {
-  if (key.x === you.x && key.y === you.y) {
+  if (isYou(key.x, key.y)) {
     you.hasKey = true;
     keyObj = null;
     entitiesToDelete.push(key);
   }
 }
 
+function updateExit(exit) {
+  if (isYou(exit.x, exit.y) && you.hasKey) {
+    // is this where to make a new level?
+    you.hasKey = false;
+    level++;
+    buildLevel(level);
+  }
+}
+
 var RANDOM_MOVES = [[1, 0], [-1, 0], [0, 1], [0, -1], [0, 0]];
 function updateMoveRandom(entity) {
   var delta = choose(RANDOM_MOVES);
-  if (isEmpty(entity.x + delta[0], entity.y + delta[1])) {
+  var width = entityWidth(entity);
+  var height = entityHeight(entity);
+  if (!isWall(entity.x + delta[0], entity.y + delta[1], width, height)
+      && entitiesAt(entity.x + delta[0], entity.y + delta[1], width, height, entity).length === 0) {
     entity.x += delta[0];
     entity.y += delta[1];
   }
   // todo maybe hurt player
+  if (isYou(entity.x, entity.y, width, height)) {
+    you.health--; // maybe give player its own update function
+  }
 }
 
 // end entity update functions //
